a7ef035545fc030098ad853351edf6d349cb7c59
KishorNaik <kishor.naik011.net@gmail.com>
2015-11-27 00:30:19 +0530

    Calling Await method in non Async method

---
+++ Sol_Await_Not_Async_Method/Sol_Await_Not_Async_Method/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_Await_Not_Async_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 
+            Task.Run(async() =>
+            {
+                await SetStringMsg();
+            }).Wait();
+
+            Task.Run(() => System.Console.WriteLine("Hello Async Direct")).Wait();
+
+            #endregion
+
+
+            #region Task Return 
+
+            Task.Run(async() =>
+            {
+                String Str = await SetStringMsg("Kishor", "Naik");
+                System.Console.WriteLine(Str);
+            }).Wait();
+
+           Task.Run(() =>
+           {
+               System.Console.WriteLine("{0} {1}", "Kishor", "Naik");
+           }).Wait();
+
+            #endregion
+
+            RunTask(() => { System.Console.WriteLine("Kishor Naik"); });
+
+        }
+
+        private static Task SetStringMsg()
+        {
+            return Task.Run(() => System.Console.WriteLine("Hello Async"));
+        }
+
+
+        private static Task<String> SetStringMsg(String FirstName, String LastName)
+        {
+            return Task.Run<String>(() =>
+            {
+                return String.Format("{0} {1}", FirstName, LastName);
+            });
+        }
+
+        private static void RunTask(Action ActionObj)
+        {
+            Task.Run(ActionObj).Wait();
+        }
+    }
+}
